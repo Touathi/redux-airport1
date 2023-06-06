2cc6d63a36a8b2611fdd257de38f982f9e966040
Touathi Vang <Touathi@outlook.com>
2023-06-06 16:50:57 -0500

    finish base

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,77 @@
 import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 function App() {
+
+  const dispatch = useDispatch()
+
+  const reduxStore = useSelector( store => store)
+  const airlineArray = useSelector( store => store.airlineArray)
+  const airplanes = useSelector( store => store.airplanes)
+
+  const [ newAirline, setNewAirline ] = useState('')
+  const [ newAirplanes, setNewAirplanes] = useState('')
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+
+    dispatch(
+            {
+              type: 'ADD_AIRLINE',
+              payload: newAirline
+            }
+    )
+      setNewAirline('')
+  }
+
+  const submitAirplanes = (evt) => {
+    evt.preventDefault();
+    dispatch(
+      {
+        type: 'ADD_AIRPLANES',
+        payload: newAirplanes
+      }
+    )
+      setNewAirplanes('')
+  }
 
   return (
     <div>
       <h1>Redux Airport</h1>
-      <input placeholder='Airline Name' />
-      <button>Add Airline</button>
-      <table>{/* Airlines should be listed here */}</table>
+      <form onSubmit={handleSubmit}>
+        <input 
+          type="text"
+          placeholder='Airline Name'
+          value={newAirline}
+          onChange={evt => setNewAirline(evt.target.value)}
+        />
+        <button type='submit'>Add Airline</button>
+      </form>
+
+
+      <pre>{JSON.stringify(reduxStore)}</pre>
+      <p>the array is {reduxStore.airlineArray} </p>
+
+      <table>{/* Airlines should be listed here */}
+        <thead>
+          <tr>
+            <th>
+            Airlines
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          
+           {airlineArray.map((airline, i) => (
+            <tr>
+            <td key={i}>
+              {airline}
+            </td>
+            </tr>
+            ))}
+          
+        </tbody>
+      </table>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,21 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
-
-/** TODO: import REDUX **/
+// Import Redux Necessitites
+import { applyMiddleware, createStore, combineReducers } from 'redux';
+import logger from 'redux-logger'
+import { Provider } from 'react-redux';
 
 
 /** TODO: Add REDUCERS */
+const airlineArray = ( state = [], action) => {
+    console.log(`airline is`, action);
+
+    if ( action.type === 'ADD_AIRLINE') {
+        console.log(`The airline added was ${action.payload}`);
+        return [...state, action.payload]
+    }
+    return state
+}
 
 
 /** TODO: Create store */
+const storeInstance = createStore (
+    combineReducers(
+        {
+            airlineArray,
 
+        }
+),
+    applyMiddleware(
+        logger
+    )
+)
 
 
 // Be sure to add the Provider! Just wrap App with it. Don't copy and paste from lecture, that will cause issues.
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <Provider store={storeInstance}>
+            <App />    
+        </Provider>
     </React.StrictMode>
 );
